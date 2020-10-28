21380cf1c433809971bb3045814df1b209751656
huuthang2509 <56929606+huuthang2509@users.noreply.github.com>
2020-10-28 21:05:58 +0700

    Command and clean code

--- search_algorithm.py
+++ search_algorithm.py
@@ -2,7 +2,7 @@ import pygame
 import graphUI
 import time
 import math
-from queue import PriorityQueue
+from queue import PriorityQueue     # Dùng thêm PriorityQueue từ thư viện queue
 from node_color import white, yellow, black, red, blue, purple, orange, green
 
 """
@@ -21,41 +21,55 @@ def BFS(graph, edges, edge_id, start, goal):
     graph[start][3] = orange
     graph[goal][3] = purple
     graphUI.updateUI()
+    # Set màu cho node Start và Goal
+
     # if edge_id(0,2) == edge_id(2,0):
     #     print("That true bro!")
 
 
-    list_path = [[start]]
-    while graph[goal][2] != white:
-        path = list_path.pop(0)
-        temp_node = path[-1]
-        if graph[temp_node][3] != blue:
+    list_path = [[start]] # list_path là mảng lưu lại đường đi từ node Start đến các node (chứa các new_path)
+
+    while graph[goal][2] != white:      # Lặp đến khi tìm thấy node Goal
+        path = list_path.pop(0)     # Lần lượt pop từng path ra để duyệt
+        temp_node = path[-1]        # Node chưa được duyệt đến
+        if graph[temp_node][3] != blue:     # Nếu node có màu blue = node đã đc duyệt
             graph[temp_node][3] = yellow
             graph[temp_node][2] = white
             graphUI.updateUI()
+            # Set màu cho node đang duyệt
+
             restructed_node = list(graph[temp_node][1])
             for adjacency_node in graph[temp_node][1]:
                 if graph[adjacency_node][3] != black and adjacency_node != goal:
                     restructed_node.remove(adjacency_node)
+            # Chỉ duyệt những node chưa tìm thấy (để loại đi trường hợp có cùng node con)
+
             for adjacency_node in restructed_node:
                 graph[adjacency_node][3] = red
                 graph[adjacency_node][2] = white
                 edges[edge_id(temp_node, adjacency_node)][1] = white
                 graphUI.updateUI()
+                # Set màu cho node và cạnh đc tìm thấy
+
                 new_path = list(path)
                 new_path.append(adjacency_node)
                 list_path.append(new_path)
-                if adjacency_node == goal:
+                # Lưu lại đường đi đến node tìm thấy vào new_path, sau đó thêm vào list_path
+
+                if adjacency_node == goal:      # Nếu tìm thấy Goal thì return path và thoát vòng while
                     result_path = new_path
                     break          
             graph[temp_node][3] = blue
             graphUI.updateUI()
+            # Set màu xanh để đánh dấu là đã duyệt qua node
+
     # print(result_path)
     for i in range (0, len(result_path) - 1):
         edges[edge_id(result_path[i], result_path[i+1])][1] = green
     graph[start][3] = orange
     graph[goal][3] = purple
     graphUI.updateUI()
+    # Update màu cho kết quả
         
     print("Implement BFS algorithm.")
     pass
@@ -70,34 +84,34 @@ def DFS(graph, edges, edge_id, start, goal):
     graph[start][3] = orange
     graph[goal][3] = purple
     graphUI.updateUI()
+    # Set màu cho node Start và Goal
 
-    previous_node = []
-    for node in range(0, len(graph)):
-        previous_node.append((node, None))
-    previous_node = dict(previous_node) 
-    previous_node[0] = -1
+    previous_node = {}      # previous_node dùng để lưu lại node cha của từng node
+    previous_node.update({start: -1})       # Node gốc ko có node cha
 
     def _dfs(curr_node):
         
         for adjacency_node in graph[curr_node][1]:
-            if previous_node[adjacency_node] == None:
+            if adjacency_node not in previous_node:       # Kiểm tra xem có phải node lá ko, nếu là lá thì ko duyệt
                 graph[adjacency_node][3] = red
                 graph[adjacency_node][2] = white
                 edges[edge_id(curr_node, adjacency_node)][1] = white
                 graphUI.updateUI()
+                # Set màu cho các node đã tìm thấy
+
                 graph[adjacency_node][3] = yellow
-                previous_node[adjacency_node] = curr_node
-                if adjacency_node == goal:
+                graphUI.updateUI()
+                # Set màu cho node(path) đang đc duyệt
+
+                previous_node.update({adjacency_node:curr_node})
+                if adjacency_node == goal:      # Nếu tìm thấy Goal thì dừng, còn không thì tiếp tục duyệt node con của nó
                     return True
-                elif _dfs(adjacency_node):
+                elif _dfs(adjacency_node):      # (tiếp tục duyệt node con)
                     return True
         graph[curr_node][3] = blue
         graphUI.updateUI()
-                    # for discovered_node in previous_node:
-                    #     if previous_node[discovered_node] != None:
-                    #         graph[discovered_node][3] = blue
-                    # graphUI.updateUI()
-                    # return True
+        # Set màu blue để đánh dấu đã duyệt qua node
+
         return False
                 
     if _dfs(start):
@@ -108,20 +122,20 @@ def DFS(graph, edges, edge_id, start, goal):
             result_path.append(int(temp_node))
             temp_node = previous_node[temp_node]
         result_path.append(start)
+        # Dựa previous_node để đưa ra path khi đi ngược từ Goal
+
         for i in range (0, len(result_path) - 1):
             edges[edge_id(result_path[i], result_path[i+1])][1] = green
-
         graph[start][3] = orange
         graph[goal][3] = purple
         graphUI.updateUI()
-    else:
-        print("Khong co duong di tu Node", start, "den", goal)
+        # Update màu cho kết quả
 
     print("Implement DFS algorithm.")
     pass
 
 
-def distance(graph, node_1, node_2):
+def distance(graph, node_1, node_2):        # Trọng số của cạnh (chi phí đường đi) = khoảng cách giữa các node trên mặt phẳng tọa độ
     x_1, y_1 = graph[node_1][0]
     x_2, y_2 = graph[node_2][0]
     return math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
@@ -131,33 +145,20 @@ def UCS(graph, edges, edge_id, start, goal):
     Uniform Cost Search search
     """
     # TODO: your code
-    # def distance(node_1, node_2):
-    #     return math.sqrt((graph[node_1][0][0] - graph[node_2][0][0])**2 + (graph[node_1][0][1] - graph[node_2][0][1])**2)
     
     graph[start][2] = white
     graph[start][3] = orange
     graph[goal][3] = purple
     graphUI.updateUI()
-    
-    visited_node = []
-    ucs_node = {start:[start, 0]}
+    # Set màu cho node Start và Goal
+
+    visited_node = []       # Lưu các node đã duyệt qua
+    ucs_node = {start:[start, 0]}       # Lưu lại node 'x', node cha của 'x', chi phí đi từ Start đến 'x'
     ucs_node_temp = PriorityQueue()
-    # def sort_ucs():
-    #     if len(ucs_node_temp) > 1:
-    #         for i in range (0, len(ucs_node_temp) - 1):
-    #             for j in range(1, len(ucs_node_temp)):
-    #                 if ucs_node_temp[i][2] > ucs_node_temp[j][2]:
-    #                     temp = ucs_node_temp[i][2]
-    #                     ucs_node_temp[i][2] = ucs_node_temp[j][2]
-    #                     ucs_node_temp[j][2] = temp
-        # else:
-        #     return
-
-
     ucs_node_temp.put((0, start))
-    while 1:
-        # print("mmmmmmmmmmmmmmmm")
-        
+    # ucs_node_temp là queue dùng để lưu lại các node và chi phí để đi đến node đó từ Start, sau đó quêu sẽ tự động sort lại theo thứ tự tăng dần về chi phí đường đi
+
+    while 1: 
         temp = ucs_node_temp.get()
         print(ucs_node)
         print(temp)       
@@ -172,6 +173,8 @@ def UCS(graph, edges, edge_id, start, goal):
                 edges[edge_id(curr_node, adjacency_node)][1] = white
                 graph[adjacency_node][3] = red
                 graphUI.updateUI()
+
+                # Nếu node chưa có trong ucs_node thì thêm vào hoặc nếu tìm đc đường đi ngắn hơn đến node đó thì update lại đường đi
                 if (adjacency_node not in ucs_node) or (ucs_node[adjacency_node][1] > ucs_node[curr_node][1] + distance(graph, curr_node, adjacency_node)):
                     ucs_node.update({adjacency_node: [curr_node, ucs_node[curr_node][1] + distance(graph, curr_node, adjacency_node)]})
                     ucs_node_temp.put((ucs_node[curr_node][1] + distance(graph, curr_node, adjacency_node), adjacency_node))
@@ -179,7 +182,8 @@ def UCS(graph, edges, edge_id, start, goal):
         visited_node.append(curr_node)
         graph[curr_node][3] = blue
         graphUI.updateUI()
-    
+        # Set màu blue cho node đã được duyệt
+
     result_path = [goal]
     temp = goal
     while temp != start:
@@ -187,21 +191,21 @@ def UCS(graph, edges, edge_id, start, goal):
                 if node_temp == temp:
                     temp = ucs_node[node_temp][0]
                     result_path.append(temp)
+                    # Tìm path từ Goal dựa vào dict ucs_node
+
     for i in range (0, len(result_path) - 1):
         edges[edge_id(result_path[i], result_path[i+1])][1] = green
-
     graph[start][3] = orange
     graph[goal][3] = purple
     graphUI.updateUI()
-        # print(visited_node)
-        # print(ucs_node)
-        # print("abcdef")
+    # Update màu cho kết quả
+
 
     print("Implement Uniform Cost Search algorithm.")
     pass
 
 
-def AStar(graph, edges, edge_id, start, goal):
+def AStar(graph, edges, edge_id, start, goal):      # Gần giống như ucs, chỉ là thêm hàm heristic
     """
     A star search
     """
@@ -210,14 +214,15 @@ def AStar(graph, edges, edge_id, start, goal):
     graph[start][3] = orange
     graph[goal][3] = purple
     graphUI.updateUI()
-    
+    # Set màu cho node Start và Goal
+
     visited_node = []
     ucs_node = {start:[start, 0]}
     ucs_node_temp = PriorityQueue()
     ucs_node_temp.put((0, start))
     while 1:
-        # Ham heuristic h(n) = khoang cach tu truc tiep (euclide) tu node dang xet den node goal
-        # -> f(n) = g(n) + h(n) = tong duong di tu start den node (n) + khoang cach truc tiep (euclide) tu node dang xet den node goal
+        # Hàm heuristic h(n) = khoảng cách trực tiếp (euclide) từ node đang xét đến node goal trên mặt phẳng tọa độ
+        # -> f(n) = g(n) + h(n) = tổng đường đi từ start đến node (n) + khoảng cách trực tiếp (euclide) từ node đang xét đến node goal trên mặt phẳng tọa độ
         temp = ucs_node_temp.get()
         print(ucs_node)
         print(temp)       
@@ -254,6 +259,7 @@ def AStar(graph, edges, edge_id, start, goal):
     graph[start][3] = orange
     graph[goal][3] = purple
     graphUI.updateUI()
+    # Update màu cho kết quả
 
     print("Implement A* algorithm.")
     pass
